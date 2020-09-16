7d084159a2136d1512f2f1a86eab0917a604f5bc
ETursunov <eldor1999@protonmail.com>
2020-09-16 20:42:25 +0500

    added some changes

--- resizer.py
+++ resizer.py
@@ -1,5 +1,17 @@
+import sys
+import os
+import random
 from PIL import Image
 
-img = Image.open('./image/pikachu.jpg')
-resize = img.resize((300,300))
-resize.save('resized.jpg','JPEG')
+image_file = sys.argv[1]
+height = sys.argv[2]
+width = sys.argv[3]
+random = random.randint(0,1000)
+
+if not os.path.exists('new'):
+	os.makedirs('new')
+
+
+img = Image.open(f'{image_file}')
+resize = img.resize((int(height),int(width)))
+resize.save(f'new/resized{random}.jpg','JPEG')
